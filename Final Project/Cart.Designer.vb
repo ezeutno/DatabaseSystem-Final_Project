3ff32499232e9a8cd0f604bec7c2471214a6ef79
ezeutno <ivan.suratno@gmail.com>
2018-06-01 17:09:35 +0700

    "Fix Cart Show"

--- Final Project/Cart.Designer.vb
+++ Final Project/Cart.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Cart
+Partial Class TransCart
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -29,21 +29,23 @@ Partial Class Cart
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(177, 149)
+        Me.Label1.Location = New System.Drawing.Point(133, 121)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(429, 91)
+        Me.Label1.Size = New System.Drawing.Size(344, 73)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "CART WIP"
         '
-        'Cart
+        'TransCart
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(600, 366)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
-        Me.Name = "Cart"
+        Me.Name = "TransCart"
         Me.Text = "Shopping Cart"
         Me.ResumeLayout(False)
         Me.PerformLayout()
--- Final Project/Cart.vb
+++ Final Project/Cart.vb
@@ -1,9 +1,9 @@
-﻿Public Class Cart
+﻿Public Class TransCart
     Private Sub Cart_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Main.Enabled = False
     End Sub
+
     Private Sub Cart_Closing(sender As Object, e As FormClosingEventArgs) Handles Me.Closing
-        Me.Dispose()
         Main.Enabled = True
         Main.Focus()
     End Sub
